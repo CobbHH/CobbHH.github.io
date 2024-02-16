de510767bceaca4d620bb8205253cf930e368d38
CobbHH <159601393+CobbHH@users.noreply.github.com>
2024-02-16 13:53:45 -0600

    Add files via upload

--- computer/src/main.js
+++ computer/src/main.js
@@ -1,1 +1,5 @@
+import { Application } from '@splinetool/runtime';
 
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/QllD0-aI-CpHmD6a/scene.splinecode');
